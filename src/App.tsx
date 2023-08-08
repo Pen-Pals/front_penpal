060079918cfdb51c8e528284a283728860d56f79
Byeonggyu Shin <94808683+byeonggyu-shin@users.noreply.github.com>
2023-08-09 01:28:35 +0900

    feat: 레이아웃 설정 및 Mail 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+
+import { Layout } from "./utils/layout/Layout";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import "./index.css";
 import Search from "./pages/Search";
 import Globesample from "./pages/Globesample";
+import Mail from "./pages/Mail";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-      {/* 샘플 페이지 */}
-      <Route path="/three" element={<Globesample/>}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        {/* 샘플 페이지 */}
+        <Route path="/three" element={<Globesample />} />
 
-      {/* 검색 페이지 */}
-      <Route path="/search" element={<Search />} />
-    </Routes>
+        {/* 검색 페이지 */}
+        <Route path="/search" element={<Search />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Mail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
