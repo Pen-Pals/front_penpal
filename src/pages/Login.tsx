cd433f103c08f605ca90c1d168cb26c626889b49
hodee <tlsghk8619@gmail.com>
2023-08-08 18:17:43 +0900

    feat: 로그인 페이지 생성

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,39 @@
-
 const Login = () => {
   return (
-    <div>
-      login page
+    <div className="w-screen h-screen flex justify center items-center">
+      <div className="w-1/2 text-3xl text-center font-thin">
+        "지구본" 혹은 "메일" 이 들어갈 것입니당
+      </div>
+
+      <div className="w-1/2 h-2/3 flex-col justify-center items-center border-2 rounded-lg">
+        <div className="h-1/3 flex items-center justify-center">
+          <h1 className="text-6xl font-thin text-center"> MaeilMail </h1>
+        </div>
+        <div className="h-1/2 flex flex-col items-center justify-center">
+          {/* 아이디 */}
+          <div className="flex flex-row mb-5">
+            <p className="font-thin text-3xl mr-10">아이디</p>
+            <input
+              className="border-2 rounded-xl text-center text-2xl font-thin"
+              type="email"
+              placeholder="id"
+            />
+          </div>
+
+          <div className="flex flex-row mb-10">
+            <p className="font-thin text-3xl mr-5">비밀번호</p>
+            <input
+              className="border-2 rounded-xl text-2xl font-thin text-center"
+              type="password"
+              placeholder="password"
+            />
+          </div>
+          <div>
+            <button className="border-2 rounded-lg text-2xl px-4 py-1 font-thin">로그인</button>
+          </div>
+        </div>
+        <p className="text-center font-thin">회원가입하기 |  ID,PW 찾기</p>
+      </div>
     </div>
   );
 };
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,16 @@
 const Signup = () => {
   return (
     <div>
-      sign up page
+      <div>
+        <h1 className="text-3xl font-bold"> 회원가입 </h1>
+        <div> 이메일 </div>
+        <input type="email" />
+        <div> 비밀번호 </div>
+        <input type="password" />
+        <div>
+          <button>로그인하기</button>
+        </div>
+      </div>
     </div>
   );
 };
