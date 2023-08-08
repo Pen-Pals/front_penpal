099471b8fefdced6c6506b6e3b86e262a22c6aaf
SeryeongK <99seryoung@naver.com>
2023-08-08 19:18:59 +0900

    feat: 검색 페이지 추가

---
+++ src/pages/Search.tsx
@@ -0,0 +1,11 @@
+/**
+ *@file Search.tsx
+ *@author @SeryeongK
+ *@date 2023-08-08
+ *@brief 친구를 탐색(위치, 닉네임, 관심사 검색 / 친구 추천)하는 페이지
+ */
+
+const Search = () => {
+  return <></>;
+};
+export default Search;
