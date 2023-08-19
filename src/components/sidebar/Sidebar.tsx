246b6d7a286ba3e160a462f2497046bcf395baf1
hodee <tlsghk8619@gmail.com>
2023-08-19 13:58:57 +0900

    feat: Sidebar 컴포넌트 Hover 기능 추가

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { BiMailSend } from "react-icons/bi";
+import { FaUserFriends, FaUsers } from "react-icons/fa";
 
 const Sidebar = () => {
   const [hovered, setHovered] = useState(false);
@@ -7,25 +9,54 @@ const Sidebar = () => {
     <div
       onMouseEnter={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}
-      className={`transition-all duration-300 ease-in-out h-screen flex items-start ${
+      className={`transition-all duration-300 ease-in-out h-screen flex flex-col items-start ${
         hovered ? "w-64" : "w-12"
-      } bg-gray-200`}
+      }`}
     >
-      <div className="w-12">
-        {/* Replace these with your icons */}
-        <div className="p-2">Icon 1</div>
-        <div className="p-2">Icon 2</div>
-        <div className="p-2">Icon 3</div>
+      <div className="flex items-center w-full h-16">
+        {/* Icon */}
+        <button className="p-2 w-12 h-full flex items-center justify-center">
+          <BiMailSend size={30} />
+        </button>
+        {/* Additional content */}
+        <div
+          className={`transition-all ease-in-out flex-grow pl-2 ${
+            hovered ? "opacity-100 delay-300" : "opacity-0"
+          }`}
+        >
+          지구본 메일함
+        </div>
       </div>
 
-      {/* Display additional content if hovered */}
-      {hovered && (
-        <div className="pl-2">
-          <div className="p-2">More content 1</div>
-          <div className="p-2">More content 2</div>
-          <div className="p-2">More content 3</div>
+      <div className="flex items-center w-full h-16">
+        {/* Icon */}
+        <button className="p-2 w-12 h-full flex items-center justify-center">
+          <FaUserFriends size={30} />
+        </button>
+        {/* Additional content */}
+        <div
+          className={`transition-all ease-in-out flex-grow pl-2 ${
+            hovered ? "opacity-100 delay-300" : "opacity-0"
+          }`}
+        >
+          내 친구 찾기
         </div>
-      )}
+      </div>
+
+      <div className="flex items-center w-full h-16">
+        {/* Icon */}
+        <button className="p-2 w-12 h-full flex items-center justify-center">
+          <FaUsers size={30} />
+        </button>
+        {/* Additional content */}
+        <div
+          className={`transition-all ease-in-out flex-grow pl-2 ${
+            hovered ? "opacity-100 delay-300" : "opacity-0"
+          }`}
+        >
+          새로운 친구 탐색하기
+        </div>
+      </div>
     </div>
   );
 };
